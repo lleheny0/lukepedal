5624deff9f2de4cf84901ec0ba8fe8771e3ee989
Luke Leheny <lleheny@umass.edu>
2017-11-09 19:35:34 -0500

    making some big changes here at corporate

--- src/components/Looper.js
+++ src/components/Looper.js
@@ -1,13 +1,13 @@
-import React, { Component, Button } from 'react'
+import React, { Component } from 'react'
 
 class Looper extends Component {
   render() {
     return (
-      <div>
-
+      <div className='looper'>
+        <button className='playback-toggle'/>
       </div>
     )
   }
 }
 
-export default Looper;
+export default Looper
