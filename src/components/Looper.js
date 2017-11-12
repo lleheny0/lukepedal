952d00a83bd7fa70e4a2a927e568e8c350662449
Luke Leheny <lleheny@umass.edu>
2017-11-12 02:43:21 -0500

    adding Tone.js

--- src/components/Looper.js
+++ src/components/Looper.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import Tone from 'tone'
 
 class Looper extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      playing: false
+    }
+  }
+
+  componentDidMount() {
+    Tone.Transport.start()
+  }
+
+  handlePlaybackToggle() {
+  }
+
   render() {
+    let { steps, currentStep, playing } = this.state
     return (
       <div className='looper'>
-        <button className='playback-toggle'/>
+        <div className='step-counter'>
+          {Tone.Transport.position}
+        </div>
+        <button
+          className='playback-toggle'
+          onClick={() => this.handlePlaybackToggle()}>
+          { playing ? 'Stop' : 'Play' }
+        </button>
       </div>
     )
   }
 }
 
 export default Looper
+
+// pattern
+// current step in pattern
+// whether looper is playing
+
+// AudioContext > setInterval
