7cf510341c6ef5334f866c78b3957cbf9ef6bf63
Luke Leheny <lleheny@umass.edu>
2017-11-14 18:37:23 -0500

    style

--- src/reducers/grid.js
+++ src/reducers/grid.js
@@ -7,7 +7,11 @@ const initialGrid = []
 for (let i = 0; i < 5; i++) {
   initialGrid[i] = []
   for (let j = 0; j < 8; j++) {
-    initialGrid[i][j] = false;
+    let rand = Math.random() * 2
+    if (rand < 1)
+      initialGrid[i][j] = false
+    else
+      initialGrid[i][j] = true
   }
 }
 
