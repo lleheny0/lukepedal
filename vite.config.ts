392695c06f20fa6ea0c2c1344bd64798312db660
Luke Leheny <luke.leheny@pm.me>
2025-06-01 10:02:45 -0400

    Add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react"
 import * as path from "node:path"
 import { defineConfig } from "vitest/config"
+import tailwindcss from "@tailwindcss/vite"
 import packageJson from "./package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 
   server: {
     open: true,
