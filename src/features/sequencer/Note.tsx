076b4d074302b2b9c11b58da55de29ab070de930
Luke Leheny <luke.leheny@pm.me>
2025-06-02 16:56:58 -0400

    Remove note labels

--- src/features/sequencer/Note.tsx
+++ src/features/sequencer/Note.tsx
@@ -19,8 +19,6 @@ export const Note = ({
       className={`${isActive ? "bg-blue-500 hover:bg-blue-400 shadow-[0px_0px_0.5rem_rgba(0,128,255,1)]" : "bg-gray-700 hover:bg-gray-600"} rounded-lg h-16 flex items-center justify-center cursor-pointer transition-colors`}
       onClick={handleToggle}
       onMouseEnter={onMouseEnter}
-    >
-      {isActive ? "ON" : "OFF"}
-    </div>
+    />
   )
 }
