5d7025c9f57945deb37c4284baf9235f285bfd8a
Luke Leheny <luke.leheny@pm.me>
2025-06-03 09:51:38 -0400

    Update Note styles

--- src/features/sequencer/Note.tsx
+++ src/features/sequencer/Note.tsx
@@ -13,10 +13,23 @@ export const Note = ({
   handleToggle,
   onMouseEnter,
 }: NoteProps) => {
+  const conditionalStyles = isActive
+    ? "bg-blue-500 hover:bg-blue-400 active:bg-blue-300 shadow-[0px_0px_0.5rem_rgba(0,128,255,1)]"
+    : "bg-gray-700 hover:bg-gray-600 active:bg-gray-500"
+
   return (
     <div
       key={`${String(pitch)}-${String(step)}`}
-      className={`${isActive ? "bg-blue-500 hover:bg-blue-400 shadow-[0px_0px_0.5rem_rgba(0,128,255,1)]" : "bg-gray-700 hover:bg-gray-600"} rounded-lg h-16 flex items-center justify-center cursor-pointer transition-colors`}
+      className={`
+        rounded-lg
+        h-16
+        flex
+        items-center
+        justify-center
+        cursor-pointer
+        transition-colors
+        ${conditionalStyles}
+      `}
       onClick={handleToggle}
       onMouseEnter={onMouseEnter}
     />
